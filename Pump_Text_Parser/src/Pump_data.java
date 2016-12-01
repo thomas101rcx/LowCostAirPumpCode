5b1bb0e0c89f9223a667e4463032403e01a23e64
Thomas Lai <thomaslai@Thomass-MacBook.local>
2016-12-01 00:54:41 -0500

    12/01/2016Commit2

---
+++ Pump_Text_Parser/src/Pump_data.java
@@ -0,0 +1,15 @@
+
+public class Pump_data {
+	private String YearMonthDate ;
+	private String HourMinuteSecond ;
+	private float flowrate;
+	private int counter;
+	public Pump_data(String YearMonthDate, String HourMinuteSecond, float flowrate, int counter){
+		this.YearMonthDate = YearMonthDate;
+		this.HourMinuteSecond = HourMinuteSecond;
+		this.flowrate = flowrate;
+		this.counter = counter; 
+	}
+	
+	
+}
---
+++ Pump_Text_Parser/src/Text_Parser.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+public class Text_Parser {
+	
+	
+
+	
+	public static void main(String [] Args){
+		ArrayList<Pump_data> DataList = new ArrayList<>();
+		try{
+        BufferedReader br = new BufferedReader(new FileReader("HighFlow.txt"));
+        String fileRead = br.readLine();
+        while (fileRead != null)
+        {
+            String[] token = fileRead.split(" ");
+        	
+    		float a;
+    		int b;
+    		String s1,s2, s3;	
+    		if(token[0].substring(0, 4).equals("High") ||token[0].substring(0, 3).equals("Low")){		
+    			 s3 = token[0].toString();
+    		}
+    		else{
+    			 a = Float.parseFloat(token[2].toString());
+    			 b = Integer.parseInt(token[3].toString());
+    			 s1 = token[0].toString();
+    			 s2 = token[1].toString();
+    			 Pump_data data = new Pump_data(s1, s2, a, b);
+    			 DataList.add(data);	
+    		}
+
+
+            // read next line before looping
+            // if end of file reached 
+            fileRead = br.readLine();
+        }
+
+        // close file stream
+        br.close();
+        
+		}
+		
+		catch (FileNotFoundException fnfe)
+        {
+            System.out.println("file not found");
+        }
+
+        catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+		
+	}
+
+}
